c6802660ccf86a415391912b19700a75f939663d
Ali <aliyazdanpanahfard@gmail.com>
2025-11-15 16:44:47 +0330

    update

--- blog/src/app/blogs/page.tsx
+++ blog/src/app/blogs/page.tsx
@@ -3,11 +3,20 @@ import Container from "@/components/container/container";
 const Blogs = () => {
   return (
     <Container>
-       <div>
-         <h1>Blogs</h1>
-       </div>
+      <div className="grid grid-cols-4 gap-4">
+        <div className="shadow p-4">
+          <h2>title</h2>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio nemo
+            praesentium aliquam deserunt nobis architecto dignissimos deleniti
+            saepe non voluptas, corporis excepturi quo, unde ipsa laudantium
+            dicta facere nisi perferendis?
+          </p>
+        </div>
+      </div>
     </Container>
-  )
+  );
 };
 
 export default Blogs;
--- blog/src/components/navbar/Navbar.tsx
+++ blog/src/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div className="Navwrapper">
+    <div className="Navwrapper shadow">
       <Link href={"/"}> Home </Link>
       <Link href={"/blogs"}> Blog </Link>
     </div>

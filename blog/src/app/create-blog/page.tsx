8f7cfeb99d578be5c4b6d82c6a90c6a5e501415a
Ali <aliyazdanpanahfard@gmail.com>
2025-11-15 17:55:49 +0330

    Create page.tsx

---
+++ blog/src/app/create-blog/page.tsx
@@ -0,0 +1,13 @@
+import Container from "@/components/container/container";
+
+const CreateBlog = () => {
+  return (
+    <Container>
+      <div>
+        <h1>create Blog</h1>
+      </div>
+    </Container>
+  );
+};
+
+export default CreateBlog;

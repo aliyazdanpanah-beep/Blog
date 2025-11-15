22ee2b04155f96bc56f5b229cd3672250944085a
Ali <aliyazdanpanahfard@gmail.com>
2025-11-15 16:36:53 +0330

    update

--- blog/src/app/blogs/page.tsx
+++ blog/src/app/blogs/page.tsx
@@ -1,5 +1,13 @@
+import Container from "@/components/container/container";
+
 const Blogs = () => {
-  return <div className="container ">Blog</div>;
+  return (
+    <Container>
+       <div>
+         <h1>Blogs</h1>
+       </div>
+    </Container>
+  )
 };
 
 export default Blogs;

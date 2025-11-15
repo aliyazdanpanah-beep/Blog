34c68c323ce7cb6d92ea71ff482d66de08609a16
Ali <aliyazdanpanahfard@gmail.com>
2025-11-15 16:48:40 +0330

    update

--- blog/src/app/blogs/page.tsx
+++ blog/src/app/blogs/page.tsx
@@ -1,9 +1,10 @@
 import Container from "@/components/container/container";
+import "./blogs.css";
 
 const Blogs = () => {
   return (
     <Container>
-      <div className="grid grid-cols-4 gap-4">
+      <div className=" Wrapper grid grid-cols-4 gap-4">
         <div className="shadow p-4">
           <h2>title</h2>
 
@@ -14,6 +15,37 @@ const Blogs = () => {
             dicta facere nisi perferendis?
           </p>
         </div>
+        <div className="shadow p-4">
+          <h2>title</h2>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio nemo
+            praesentium aliquam deserunt nobis architecto dignissimos deleniti
+            saepe non voluptas, corporis excepturi quo, unde ipsa laudantium
+            dicta facere nisi perferendis?
+          </p>
+        </div>
+        <div className="shadow p-4">
+          <h2>title</h2>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio nemo
+            praesentium aliquam deserunt nobis architecto dignissimos deleniti
+            saepe non voluptas, corporis excepturi quo, unde ipsa laudantium
+            dicta facere nisi perferendis?
+          </p>
+        </div>
+        <div className="shadow p-4">
+          <h2>title</h2>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio nemo
+            praesentium aliquam deserunt nobis architecto dignissimos deleniti
+            saepe non voluptas, corporis excepturi quo, unde ipsa laudantium
+            dicta facere nisi perferendis?
+          </p>
+        </div>
+        
       </div>
     </Container>
   );

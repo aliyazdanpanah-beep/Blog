26953360ddeb8ab829436dce12aaee2e5311e601
Ali <aliyazdanpanahfard@gmail.com>
2025-11-15 17:20:51 +0330

    dynamic Articel

--- blog/src/app/blogs/page.tsx
+++ blog/src/app/blogs/page.tsx
@@ -1,18 +1,29 @@
 import Container from "@/components/container/container";
 import "./blogs.css";
 import Articel from "@/components/Articels/Articel";
+import axios from "axios";
 
-const Blogs = () => {
+export interface IGetArticel {
+  id: number,
+  title: string,
+  description: string,
+}
+
+async function Blogs() {
+  const result = await fetch("http://localhost:8001/Articels");
+  const data = (await result.json()) as IGetArticel[]
+
   return (
     <Container>
       <div className=" Wrapper grid grid-cols-4 gap-4">
-        <Articel />      
-        <Articel />      
-        <Articel />      
-        <Articel />      
+       {
+        data.map((item) => (
+          <Articel />
+        ))
+       }
       </div>
     </Container>
   );
-};
+}
 
 export default Blogs;

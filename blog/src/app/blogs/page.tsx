284256bfb0a21a7b6959687a6306f4842811efc4
Ali <aliyazdanpanahfard@gmail.com>
2025-11-15 17:26:30 +0330

    Update page.tsx

--- blog/src/app/blogs/page.tsx
+++ blog/src/app/blogs/page.tsx
@@ -4,23 +4,21 @@ import Articel from "@/components/Articels/Articel";
 import axios from "axios";
 
 export interface IGetArticel {
-  id?: number,
-  title?: string,
-  description?: string,
+  id?: number;
+  title?: string;
+  description?: string;
 }
 
 async function Blogs() {
   const result = await fetch("http://localhost:8001/Articels");
-  const data = (await result.json()) as IGetArticel[]
+  const data = (await result.json()) as IGetArticel[];
 
   return (
     <Container>
       <div className=" Wrapper grid grid-cols-4 gap-4">
-       {
-        data.map((item) => (
-          <Articel {...item} />
-        ))
-       }
+        {data.map((item) => (
+          <Articel {...item} key={item.id} />
+        ))}
       </div>
     </Container>
   );

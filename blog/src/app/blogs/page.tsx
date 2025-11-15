ab8bbbea433e60fdba616fbd1663639ffe6befc1
Ali <aliyazdanpanahfard@gmail.com>
2025-11-15 16:52:12 +0330

    update

--- blog/src/app/blogs/page.tsx
+++ blog/src/app/blogs/page.tsx
@@ -1,51 +1,15 @@
 import Container from "@/components/container/container";
 import "./blogs.css";
+import Articel from "@/components/Articels/Articel";
 
 const Blogs = () => {
   return (
     <Container>
       <div className=" Wrapper grid grid-cols-4 gap-4">
-        <div className="shadow p-4">
-          <h2>title</h2>
-
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio nemo
-            praesentium aliquam deserunt nobis architecto dignissimos deleniti
-            saepe non voluptas, corporis excepturi quo, unde ipsa laudantium
-            dicta facere nisi perferendis?
-          </p>
-        </div>
-        <div className="shadow p-4">
-          <h2>title</h2>
-
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio nemo
-            praesentium aliquam deserunt nobis architecto dignissimos deleniti
-            saepe non voluptas, corporis excepturi quo, unde ipsa laudantium
-            dicta facere nisi perferendis?
-          </p>
-        </div>
-        <div className="shadow p-4">
-          <h2>title</h2>
-
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio nemo
-            praesentium aliquam deserunt nobis architecto dignissimos deleniti
-            saepe non voluptas, corporis excepturi quo, unde ipsa laudantium
-            dicta facere nisi perferendis?
-          </p>
-        </div>
-        <div className="shadow p-4">
-          <h2>title</h2>
-
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio nemo
-            praesentium aliquam deserunt nobis architecto dignissimos deleniti
-            saepe non voluptas, corporis excepturi quo, unde ipsa laudantium
-            dicta facere nisi perferendis?
-          </p>
-        </div>
-        
+        <Articel />      
+        <Articel />      
+        <Articel />      
+        <Articel />      
       </div>
     </Container>
   );
---
+++ blog/src/components/Articels/Articel.tsx
@@ -0,0 +1,16 @@
+const Articel = () => {
+  return (
+    <div className="shadow p-4">
+      <h2>title</h2>
+
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio nemo
+        praesentium aliquam deserunt nobis architecto dignissimos deleniti saepe
+        non voluptas, corporis excepturi quo, unde ipsa laudantium dicta facere
+        nisi perferendis?
+      </p>
+    </div>
+  );
+};
+
+export default Articel;

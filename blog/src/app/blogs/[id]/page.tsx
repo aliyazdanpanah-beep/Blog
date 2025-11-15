1dccdf9c8849b18b5a207b20ce424da6288c8763
Ali <aliyazdanpanahfard@gmail.com>
2025-11-15 17:34:15 +0330

    Update page.tsx

--- blog/src/app/blogs/[id]/page.tsx
+++ blog/src/app/blogs/[id]/page.tsx
@@ -1,3 +1,19 @@
-const Articel = () => {
-    
+import Container from "@/components/container/container";
+
+function Articel() {
+  return (
+    <Container>
+      <div>
+        <h1>title</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+          praesentium perferendis doloremque eveniet, dolor commodi suscipit
+          veniam blanditiis quia optio laborum non, rerum amet magni beatae cum
+          ut numquam accusantium.
+        </p>
+      </div>
+    </Container>
+  );
 }
+
+export default Articel;
